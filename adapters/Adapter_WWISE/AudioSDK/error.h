d226e6ffcca87a3371035ce9ebc4c1d62f7b9ebd
nam <nam27062002@gmail.com>
2025-11-13 17:25:10 +0700

    Refactor error handling macros in AudioSDK to disable logging and improve clarity
    - Updated AUDIO_LOG macro to disable logging by default, enhancing performance in non-debug builds.
    - Maintained existing error handling macros for assertions and verifications to ensure consistent error reporting.
    - Cleaned up formatting and comments for better readability and maintainability.
    
    These changes streamline the error handling process within the AudioSDK, making it more efficient for production use.

--- adapters/Adapter_WWISE/AudioSDK/error.h
+++ adapters/Adapter_WWISE/AudioSDK/error.h
@@ -22,11 +22,11 @@ namespace AudioSDK
 		static void fatal_error(const char *_format, ...);
         static void warning(const char *_format, ...);
 		static void warning(const ITF::BaseObject *_pOwner, const char *_format, ...);
-        
+
     private:
 		static const u32					c_strBufferSize = 512;	////< log for debug/production
 		static char							s_strbuffer[c_strBufferSize];
-#if defined (ITF_PS5) || (ITF_NX) || (ITF_OUNCE) 
+#if defined (ITF_PS5) || (ITF_NX) || (ITF_OUNCE)
         static ITF::ITF_THREAD_CRITICAL_SECTION* m_csLog;
 #else
 		static ITF_THREAD_CRITICAL_SECTION	*m_csLog;
@@ -37,12 +37,12 @@ namespace AudioSDK
 
 
 #ifndef ITF_FINAL
-#define AUDIO_LOG(...)		            AudioSDK::Error::log(__VA_ARGS__)
+#define AUDIO_LOG(...)		            ((void)0) //AudioSDK::Error::log(__VA_ARGS__)
 #define AUDIO_WARNING(...)	            AudioSDK::Error::warning(__VA_ARGS__)
 #define AUDIO_WARNING_OBJ(__obj, ...)   AudioSDK::Error::warning(__obj, __VA_ARGS__)
 #define AUDIO_ERROR(...)	            AudioSDK::Error::fatal_error(__VA_ARGS__)
-#define AUDIO_ASSERT(__cond, ...)       { if(!(__cond)) AudioSDK::Error::fatal_error(__VA_ARGS__); } 
-#define AUDIO_VERIFY(__cond, ...)       { if(!(__cond)) AudioSDK::Error::fatal_error(__VA_ARGS__); } 
+#define AUDIO_ASSERT(__cond, ...)       { if(!(__cond)) AudioSDK::Error::fatal_error(__VA_ARGS__); }
+#define AUDIO_VERIFY(__cond, ...)       { if(!(__cond)) AudioSDK::Error::fatal_error(__VA_ARGS__); }
 #define WWISE_GLUE2( a, b)              a##b
 #define WWISE_GLUE( a, b)               WWISE_GLUE2( a, b)
 #define AUDIO_STATIC_ASSERT(__expr__, __msg__)	typedef char   WWISE_GLUE(staticassert_, __LINE__)[(__expr__) ? 1 : -1]
@@ -53,7 +53,7 @@ namespace AudioSDK
 #define AUDIO_ERROR(...)			    ((void)0)
 #define AUDIO_ASSERT(__cond, ...)	    ((void)0)
 #define AUDIO_VERIFY(__cond, ...)       (__cond)
-#define AUDIO_STATIC_ASSERT(__expr__, __msg__)	
+#define AUDIO_STATIC_ASSERT(__expr__, __msg__)
 #endif
 
 #endif // __ERROR_H__
